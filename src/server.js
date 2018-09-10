dc7ee8d261729672c49efbfe6a3447e12bf48d95
Riasets <alextel2207@gmail.com>
2018-09-10 13:08:11 +0300

    working login form, added access

--- src/server.js
+++ src/server.js
@@ -3,6 +3,12 @@ const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
+
 require('./database/db');
 require('./routes')(app, {});
 

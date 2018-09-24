8d35ed4287f637f55ef1fd50fa8663727e0d0936
Riasets <alextel2207@gmail.com>
2018-09-24 13:25:44 +0300

    schedules fix

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const users = mongoose.model('User');
+const schedules = mongoose.model('UserSchedule');
 const jwt = require('jsonwebtoken');
 const secretKey = require('./env');
 
@@ -15,14 +16,15 @@ module.exports = function(app, db) {
             })
     });
     app.get('/api/login', (req, res) =>{
-        console.log(req.headers);
        users.findOne({email: req.headers['email'], password: req.headers['password']})
            .exec((err, user) =>{
+               console.log(err, user);
                if (!user){
                    res.status(404).send({error : "User not found"});
                } else {
                    schedules.findOne({owner: user._id})
                        .exec((err, schedule) => {
+                           console.log(err, schedule);
                            if (err){
                                res.status(404).send({error : "Schedule not found"});
                            } else {
--- src/routes/register.js
+++ src/routes/register.js
@@ -1,33 +1,32 @@
 const mongoose = require('mongoose');
 const users = mongoose.model('User');
+const schedules = mongoose.model('UserSchedule');
 
 module.exports = function(app, db) {
     app.post('/api/register', (req, res) => {
         if (req.body.email && req.body.password) {
             users.find({email: req.body.email})
-                .exec((err,res) => {
-                    if(res){
+                .exec((err,user) => {
+                    if(user[0] !== undefined){
                         res.status(400).send({error: "User with this email already exist"});
                     } else {
-                      newUser = {
+                      const newUser = {
                           email: req.body.email,
                           password: req.body.password,
                           isAdmin: false,
                           importSchedule: null,
                           importEvents: [],
                       }
-                      users.create(newUser);
-                      users.findOne(newUser)
-                          .exec((err, user) =>{
-                              if (err) {
-                                  res.status(400).send({error: "Database Error", message: err})
-                              } else {
-                                  schedules.create({copy: false, owner: user._id});
-                              }
-                          })
+                      users.create(newUser, (err, user) => {
+                          if (err) {
+                              res.status(400).send({error: "Database Error", message: err})
+                          } else {
+                              schedules.create({copy: false, owner: user._id});
+                              res.send("OK");
+                          }
+                      });
                     }
                 });
-            res.send("OK")
         } else {
             res.status(400).send({error: "Bad request"})
         }

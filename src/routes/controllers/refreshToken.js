3ecab3b591f422472ec9e53e16be9c16a78f979f
Alexander Kudenyuk <a.kudenyuk@belitsoft.com>
2018-11-05 11:54:58 +0300

    express-validator, error handers part

--- src/routes/controllers/refreshToken.js
+++ src/routes/controllers/refreshToken.js
@@ -3,27 +3,17 @@ const tokens = mongoose.model('RefreshToken');
 const jwt = require('jsonwebtoken');
 const secretKey = require('../env');
 
-async function refreshToken(req){
-    if (req.headers.token){
-        try {
-            const decode = jwt.verify(req.headers.token, secretKey);
-            const refreshToken = await checkToken(req.headers.token, decode);
-            const token = jwt.sign({
-                    id: decode.id,
-                    isAdmin: decode.isAdmin,
-                    schedule: decode.schedule}, secretKey);
-            return {token, refreshToken}
-        } catch(err) {
-                console.log(err);
-                throw new Error(err)
-        }
-    } else {
-        throw new Error("Token not found")
-    }
+async function refreshToken(token){
+   const decode = jwt.verify(token, secretKey);
+   const refreshToken = await checkToken(token, decode);
+   const newToken = jwt.sign({
+       id: decode.id,
+       isAdmin: decode.isAdmin,
+       schedule: decode.schedule}, secretKey);
+   return {newToken, refreshToken}
 }
-
+// TODO error handlers 
 async function checkToken(token, decode){
-    try {
         if (tokens.findOneAndDelete(token)) {
             const token = await tokens.create({
                 token: jwt.sign({
@@ -37,11 +27,6 @@ async function checkToken(token, decode){
         } else {
             throw new Error('Token is not exist');
         }
-
-    } catch(err){
-            throw new Error(err)
-    }
-
 }
 
 module.exports = refreshToken;
--- src/routes/refreshToken.js
+++ src/routes/refreshToken.js
@@ -1,18 +1,23 @@
-const mongoose = require('mongoose');
-const users = mongoose.model('User');
-const tokens = mongoose.model('RefreshToken');
-const jwt = require('jsonwebtoken');
-const secretKey = require('./env');
 const refreshToken = require('./controllers/refreshToken');
+const { header, validationResult } = require('express-validator/check');
 
 module.exports = function (app, db) {
-    app.get('/api/refresh', (req, res) => {
-        refreshToken(req)
+    app.get('/api/refresh',
+        header('token').isString().withMessage('Empty token field'),
+        (req, res) => {
+        const errors = validationResult(req);
+        console.log(errors.array());
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error: errors.array()[0] });
+        }
+
+        refreshToken(req.headers.token)
             .then((token)=>{
                 console.log(token);
                 res.send(token)
             })
             .catch((err)=>{
+                console.log(err);
                 res.status(400).send(err)
             })
     })
